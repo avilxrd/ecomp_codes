8b32c623b6f310edb793041c5ba03d6764cf52cc
Miguel Avila <miguelaviladeoliveira@gmai.com>
2023-10-09 11:10:52 -0300

    comecando aula 09-10

---
+++ Algoritmos/09-10 vetores e matrizes/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+Anotações da Aula:
+Dia 09-10-2023, Vetores e Matrizes
+
+-> Declaração de um Vetor: int v[n], para n o número do "tamanho" do vetor.
+-> v[n]: acessando o valor guardado na posição n do vetor
+
+*/
+
+int main(){
+    int i;
+
+    for(i=0; i<10; i++){
+        printf("%d", i);
+    }
+    return 0;
+}
